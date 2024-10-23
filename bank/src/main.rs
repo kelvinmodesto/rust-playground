de07d3267dc3a1e634d1093768c846d278c89da8
Kelvin Modesto <km.modesto@gmail.com>
2024-10-22 22:20:50 -0300

    Back to course

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -4,11 +4,33 @@ struct Account {
     id: u32,
     holder: String,
 }
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
 #[derive(Debug)]
 struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: Account) {
+    print!("{:#?}", account);
+}
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("Monkey D. Luffy"));
+
+    print_account(account);
+    print_account(account);
 }
