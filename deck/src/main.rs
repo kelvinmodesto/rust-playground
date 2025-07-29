7c1ae8ae0bc771b4649bfe0ff84c965a8329e264
Kelvin Modesto <km.modesto@gmail.com>
2025-07-28 23:46:29 -0300

    Finish deck project

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,5 @@
+use rand::{rng, seq::SliceRandom};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
@@ -5,13 +7,37 @@ struct Deck {
 
 impl Deck {
     fn new() -> Self {
-        Deck { cards: vec![] }
+        let suits = ["hearts", "spades", "diamonds", "clubs"];
+        let values = [
+            "ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack",
+            "queen", "king",
+        ];
+
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
     }
-    fn shuffle(&self) {}
-    fn deal(&self) {}
+
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
-    let deck = Deck::new();
-    println!("Hello, world! {:?}", deck);
+    let mut deck = Deck::new();
+    deck.shuffle();
+    // needs error handling
+    deck.deal(48);
+    println!("Hello, world! {:#?}", deck);
 }
